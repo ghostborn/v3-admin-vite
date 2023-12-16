4865e14b58062e1f4e1bed94f896721002c604c7
贾丽鹏 <1094425279@qq.com>
2023-12-16 17:17:37 +0800

    feat #hook更新

--- src/router/index.ts
+++ src/router/index.ts
@@ -130,6 +130,43 @@ export const constantRoutes: RouteRecordRaw[] = [
         }
       }
     ]
+  },
+  {
+    path: "/hook-demo",
+    component: Layouts,
+    redirect: "/hook-demo/use-fetch-select",
+    name: "HookDemo",
+    meta: {
+      title: "Hook 示例",
+      elIcon: "Menu",
+      alwaysShow: true
+    },
+    children: [
+      {
+        path: "use-fetch-select",
+        component: () => import("@/views/hook-demo/use-fetch-select.vue"),
+        name: "UseFetchSelect",
+        meta: {
+          title: "useFetchSelect"
+        }
+      },
+      {
+        path: "use-fullscreen-loading",
+        component: () => import("@/views/hook-demo/use-fullscreen-loading.vue"),
+        name: "UseFullscreenLoading",
+        meta: {
+          title: "useFullscreenLoading"
+        }
+      },
+      {
+        path: "use-watermark",
+        component: () => import("@/views/hook-demo/use-watermark.vue"),
+        name: "UseWatermark",
+        meta: {
+          title: "useWatermark"
+        }
+      }
+    ]
   }
 ]
 
